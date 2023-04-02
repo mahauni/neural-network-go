d89ef39b493b52a36780df9007dfdb13385df43e
Lucas Raoni <lucasraoniha@gmail.com>
2023-04-02 17:52:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Neural Network from scratch in golang.
+
+Taken the neural code from the blog:
+
+https://sausheong.github.io/posts/how-to-build-a-simple-artificial-neural-network-with-go/
+
+### Run it:
+
+first you will need to clone the repo and enter the folder:
+
+$ git clone https://github.com/mahauni/neural-nerwork-go
+
+$ cd neural-network-go
+
+and then:
+
+$ go build
+
+to train the neural network
+$ ./neural-network-go.exe -mnist train
+
+mass predict a bunch of tests
+$ ./neural-network-go.exe -mnist predict 
+
+
+and will add more features to it:
+
+- make a way to draw with it and try the neural network to guess.
---
+++ main.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+func main() {
+	// 784 inputs - 28 x 28 pixels, each pixel is an input
+	// 200 hidden neurons - an arbitrary number
+	// 10 outputs - digits 0 to 9
+	// 0.1 is the learning rate
+
+	net := CreateNetwork(784, 200, 10, 0.1)
+
+	mnist := flag.String("mnist", "", "Either train or predict to evaluate neural network")
+	flag.Parse()
+
+	// train or mass predict to determinate the effectiveness of the trained network
+	switch *mnist {
+	case "train":
+		mnistTrain(&net)
+		save(net)
+	case "predict":
+		load(&net)
+		mnistPredict(&net)
+	default:
+		// dont do anything
+	}
+}
+
+type Network struct {
+	inputs        int
+	hiddens       int
+	outputs       int
+	hiddenWeights *mat.Dense
+	outputWeights *mat.Dense
+	learningRate  float64
+}
+
+func dot(m, n mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	_, c := n.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Product(m, n)
+	return o
+}
+
+func apply(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Apply(fn, m)
+	return o
+}
+
+func scale(s float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Scale(s, m)
+	return o
+}
+
+func multiply(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.MulElem(m, n)
+	return o
+}
+
+func add(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Add(m, n)
+	return o
+}
+
+func subtract(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Sub(m, n)
+	return o
+}
+
+func addScalar(i float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	a := make([]float64, r*c)
+	for x := 0; x < r*c; x++ {
+		a[x] = i
+	}
+	n := mat.NewDense(r, c, a)
+	return add(m, n)
+}
+
+func CreateNetwork(input, hidden, output int, rate float64) (net Network) {
+	net = Network{
+		inputs:       input,
+		hiddens:      hidden,
+		outputs:      output,
+		learningRate: rate,
+	}
+	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
+	return
+}
+
+func randomArray(size int, v float64) (data []float64) {
+	dist := distuv.Uniform{
+		Min: -1 / math.Sqrt(v),
+		Max: 1 / math.Sqrt(v),
+	}
+	data = make([]float64, size)
+	for i := 0; i < size; i++ {
+		data[i] = dist.Rand()
+	}
+	return
+}
+
+func (net Network) Predict(inputData []float64) mat.Matrix {
+	// feedforward
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+	return finalOutputs
+}
+
+// Activation function
+func sigmoid(r, c int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
+}
+
+// Train the neural network
+func (net *Network) Train(inputData []float64, targetData []float64) {
+	// feedforward
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+	// find errors
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := subtract(targets, finalOutputs)
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+	// backpropagate
+	net.outputWeights = add(net.outputWeights,
+		scale(net.learningRate,
+			dot(multiply(outputErrors, sigmoidPrime(finalOutputs)),
+				hiddenOutputs.T()))).(*mat.Dense)
+	net.hiddenWeights = add(net.hiddenWeights,
+		scale(net.learningRate,
+			dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)),
+				inputs.T()))).(*mat.Dense)
+}
+
+func sigmoidPrime(m mat.Matrix) mat.Matrix {
+	rows, _ := m.Dims()
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(rows, 1, o)
+	return multiply(m, subtract(ones, m)) // m * (-1 - m)
+}
+
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+	o, err := os.Create("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+// load a neural network from file
+func load(net *Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+	return
+}
+
+func mnistTrain(net *Network) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	t1 := time.Now()
+
+	for epochs := 0; epochs < 5; epochs++ {
+		testFile, _ := os.Open("mnist_dataset/mnist_train.csv")
+		r := csv.NewReader(bufio.NewReader(testFile))
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+
+			inputs := make([]float64, net.inputs)
+			for i := range inputs {
+				x, _ := strconv.ParseFloat(record[i], 64)
+				inputs[i] = (x / 255.0 * 0.999) + 0.001
+			}
+
+			targets := make([]float64, 10)
+			for i := range targets {
+				targets[i] = 0.001
+			}
+			x, _ := strconv.Atoi(record[0])
+			targets[x] = 0.999
+
+			net.Train(inputs, targets)
+		}
+		testFile.Close()
+	}
+	elapsed := time.Since(t1)
+	fmt.Printf("\nTime taken to train: %s\n", elapsed)
+}
+
+func mnistPredict(net *Network) {
+	t1 := time.Now()
+	checkFile, _ := os.Open("mnist_dataset/mnist_test.csv")
+	defer checkFile.Close()
+
+	total := 0
+	score := 0
+	r := csv.NewReader(bufio.NewReader(checkFile))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		inputs := make([]float64, net.inputs)
+		for i := range inputs {
+			if i == 0 {
+				inputs[i] = 1.0
+			}
+			x, _ := strconv.ParseFloat(record[i], 64)
+			inputs[i] = (x / 255.0 * 0.999) + 0.001
+		}
+		outputs := net.Predict(inputs)
+		best := 0
+		highest := 0.0
+		for i := 0; i < net.outputs; i++ {
+			if outputs.At(i, 0) > highest {
+				best = i
+				highest = outputs.At(i, 0)
+			}
+		}
+		target, _ := strconv.Atoi(record[0])
+		if best == target {
+			score++
+		}
+		total++
+	}
+
+	elapsed := time.Since(t1)
+	fmt.Printf("Time taken to check: %s\n", elapsed)
+	fmt.Println("score:", score)
+	fmt.Println("total:", total)
+}
